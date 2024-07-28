43e73123250d380742e96ce0e3e87305b7ff9c19
SWoto <26379593+SWoto@users.noreply.github.com>
2024-07-28 14:35:10 -0300

    added return complete for stores

--- src/api/v1/endpoints/stores.py
+++ src/api/v1/endpoints/stores.py
@@ -6,12 +6,25 @@ from fastapi import APIRouter, status, Depends, HTTPException
 
 from src.models import StoresModel
 from src.schemas.stores import PlainStoreSchema, ReturnStoreSchema
+from src.schemas.nested_schemas import ReturnFullStoresSchema
 from src.core.deps import get_session, is_valid_uuid
 
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
+
+async def _get_store_by_id(id: str, db: Annotated[AsyncSession, Depends(get_session)]):
+    if not is_valid_uuid(id):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND)
+    
+    requested_store = await StoresModel.find_by_id(id, db)
+    if not requested_store:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND)
+
+    return requested_store
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=ReturnStoreSchema)
 async def post_store(store: PlainStoreSchema, db: Annotated[AsyncSession, Depends(get_session)]):
@@ -25,14 +38,9 @@ async def post_store(store: PlainStoreSchema, db: Annotated[AsyncSession, Depend
     return new_store
 
 @router.get('/{id}', response_model=ReturnStoreSchema)
-async def get_store_by_id(id: str, db: Annotated[AsyncSession, Depends(get_session)]):
-    if not is_valid_uuid(id):
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND)
-    
-    requested_store = await StoresModel.find_by_id(id, db)
-    if not requested_store:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND)
+async def get_store_by_id(store: Annotated[StoresModel, Depends(_get_store_by_id)]):
+    return store
 
-    return requested_store
+@router.get('/complete/{id}', response_model=ReturnFullStoresSchema)
+async def get_product_by_id_with_relationship(store: Annotated[StoresModel, Depends(_get_store_by_id)]):
+    return store
