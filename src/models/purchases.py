6d3907949b83b2d22168d6849445185efba433bf
SWoto <26379593+SWoto@users.noreply.github.com>
2024-07-29 10:43:12 -0300

    total_qnt to purchases

--- src/models/purchases.py
+++ src/models/purchases.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Numeric, DateTime, Boolean
+from sqlalchemy import Column, Numeric, DateTime, Boolean, Integer
 
 from src.models.base import BaseModel
 
@@ -7,12 +7,12 @@ class PurchasesModel(BaseModel):
     __tablename__ = 'purchases'
 
     total_price = Column(Numeric(precision=10, scale=2))
+    total_qnt = Column(Integer)
     paid = Column(Boolean, default=False)
     paid_on = Column(DateTime)
-
 
     def __init__(self, *args, **kwargs):
         super(PurchasesModel, self).__init__(*args, **kwargs)
 
     def __repr__(self) -> str:
-        return f"<PurchasesModel(id={self.id}, total_price={self.total_price}, paid={self.paid}, paid_on={self.paid_on}, created_on={self.created_on}, modified_on={self.modified_on})>"
+        return f"<PurchasesModel(id={self.id}, total_price={self.total_price}, total_qnt={self.total_price}, paid={self.paid}, paid_on={self.paid_on}, created_on={self.created_on}, modified_on={self.modified_on})>"
